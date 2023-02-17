f500b729bc2f51d8fc6e900b79ba41167a2ae1e5
Enock <enock.mokua@student.moringaschool.com>
2023-02-17 14:59:31 +0300

    add functionality to map component

--- src/map.js
+++ src/map.js
@@ -1,3 +1,18 @@
+import "./App.css";
+import React, { useEffect } from "react";
+import L from "leaflet";
+
 export default function Map() {
+  useEffect(() => {
+    const map = L.map("map").setView([51.505, -0.09], 13);
+
+    L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
+      attribution: "Map data &copy; OpenStreetMap contributors",
+    }).addTo(map);
+
+    const marker = L.marker([51.505, -0.09]).addTo(map);
+    marker.bindPopup("Hello, world!").openPopup();
+  }, []);
+
   return <section id="map"></section>;
 }
