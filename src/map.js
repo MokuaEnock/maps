7e20c138ec142d4beb3537672fe31f9377ea0212
Enock <enock.mokua@student.moringaschool.com>
2023-02-24 21:27:44 +0300

    remove redudant code and add react leaflet

--- src/map.js
+++ src/map.js
@@ -1,18 +1,7 @@
 import "./App.css";
-import React, { useEffect } from "react";
-import L from "leaflet";
+import React, { useState } from "react";
+import { Map, Marker, Popup, TileLayer } from "react-leaflet";
 
-export default function Map() {
-  useEffect(() => {
-    const map = L.map("map").setView([51.505, -0.09], 13);
-
-    L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
-      attribution: "Map data &copy; OpenStreetMap contributors",
-    }).addTo(map);
-
-    const marker = L.marker([51.505, -0.09]).addTo(map);
-    marker.bindPopup("Hello, world!").openPopup();
-  }, []);
-
-  return <section id="map"></section>;
+export default function Mapp() {
+  return <></>;
 }
