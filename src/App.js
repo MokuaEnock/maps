c6250ccf1651378dce3ad3d0f723985037f40ecb
Enock <enock.mokua@student.moringaschool.com>
2023-02-27 15:00:44 +0300

    grab input from form using usestate

--- src/App.js
+++ src/App.js
@@ -5,14 +5,35 @@ import Pdf from "./Components/Pdf/pdf";
 import { useState } from "react";
 
 function App() {
-  let [latitude, setLatitude] = useState("");
-  let [longitude, setLongitude] = useState("");
+  let [formData, setFormData] = useState({
+    latitude: "",
+    longitude: "",
+  });
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    console.log("printed");
+  }
 
   function Form() {
     return (
-      <form>
-        <input type="number" placeholder="Enter Latitude" />
-        <input type="number" placeholder="Enter Longitude" />
+      <form onSubmit={handleSubmit}>
+        <input
+          type="number"
+          placeholder="Enter Latitude"
+          value={formData.latitude}
+          onChange={(e) => {
+            setFormData({ ...formData, latitude: e.target.value });
+          }}
+        />
+        <input
+          type="number"
+          placeholder="Enter Longitude"
+          value={formData.longitude}
+          onChange={(e) => {
+            setFormData({ ...formData, longitude: e.target.value });
+          }}
+        />
         <button type="submit">Submit coordinates</button>
       </form>
     );
