19c247327a945fe1a44f10d99cc2e58ac56d2944
Enock <enock.mokua@student.moringaschool.com>
2023-02-27 14:39:28 +0300

    create for component

--- src/App.js
+++ src/App.js
@@ -2,10 +2,25 @@ import "./App.css";
 
 import MapDisplay from "./Components/Map/map";
 import Pdf from "./Components/Pdf/pdf";
+import { useState } from "react";
 
 function App() {
+  let [latitude, setLatitude] = useState("");
+  let [longitude, setLongitude] = useState("");
+
+  function Form() {
+    return (
+      <form>
+        <input type="number" placeholder="Enter Latitude" />
+        <input type="number" placeholder="Enter Longitude" />
+        <button type="submit">Submit coordinates</button>
+      </form>
+    );
+  }
+
   return (
     <div className="App">
+      <Form />
       <MapDisplay />
       <Pdf />
     </div>
